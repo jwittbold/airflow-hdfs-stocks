7b3c02105a7d99c680c861547dfa73c92598f74b
Jack Wittbold <jwittbold@gmail.com>
2021-08-21 12:53:59 +0800

    commit of create_tmp_dir.sh

--- scripts/create_tmp_dir.sh
+++ scripts/create_tmp_dir.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# creates current date variable for Eastern Daylight Time
+# creates current date variable based on Eastern Daylight Time
 date_EDT="$(TZ="EST5EDT" date +'%Y-%m-%d')"
 mkdir -p /tmp/data/${date_EDT}
-
-
